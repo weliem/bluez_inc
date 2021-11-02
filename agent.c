600e065508f0a44e958174e898678ead94786288
martijn <martijn.van.welie@gmail.com>
2021-11-02 20:54:27 +0100

    Fix memory leaks in Agent

--- agent.c
+++ agent.c
@@ -45,7 +45,8 @@ static void bluez_agent_method_call(GDBusConnection *conn,
 
     if (!strcmp(method, "RequestPinCode")) {
         g_variant_get(params, "(o)", &object_path);
-        log_debug(TAG, "request pincode");
+        log_debug(TAG, "request pincode for %s", object_path);
+        g_free(object_path);
 
         // add code to request pin
         pin = "123";
@@ -53,9 +54,12 @@ static void bluez_agent_method_call(GDBusConnection *conn,
     } else if (!strcmp(method, "DisplayPinCode")) {
         g_variant_get(params, "(os)", &object_path, &pin);
         log_debug(TAG, "displaying pincode %s", pin);
+        g_free(object_path);
+        g_free(pin);
         g_dbus_method_invocation_return_value(invocation, NULL);
     } else if (!strcmp(method, "RequestPasskey")) {
         g_variant_get(params, "(o)", &object_path);
+        g_free(object_path);
         Device *device = binc_adapter_get_device_by_path(adapter, object_path);
         if (device != NULL) {
             binc_device_set_bonding_state(device, BONDING);
@@ -69,9 +73,11 @@ static void bluez_agent_method_call(GDBusConnection *conn,
     } else if (!strcmp(method, "DisplayPasskey")) {
         g_variant_get(params, "(ouq)", &object_path, &pass, &entered);
         log_debug(TAG, "passkey: %u, entered: %u", pass, entered);
+        g_free(object_path);
         g_dbus_method_invocation_return_value(invocation, NULL);
     } else if (!strcmp(method, "RequestConfirmation")) {
         g_variant_get(params, "(ou)", &object_path, &pass);
+        g_free(object_path);
         log_debug(TAG, "request confirmation for %u", pass);
         g_dbus_method_invocation_return_value(invocation, NULL);
     } else if (!strcmp(method, "RequestAuthorization")) {
@@ -95,6 +101,8 @@ static void bluez_agent_method_call(GDBusConnection *conn,
     } else if (!strcmp(method, "AuthorizeService")) {
         g_variant_get(params, "(os)", &object_path, &uuid);
         log_debug(TAG, "authorize service");
+        g_free(object_path);
+        g_free(uuid);
         g_dbus_method_invocation_return_value(invocation, NULL);
     } else if (!strcmp(method, "Cancel")) {
         log_debug(TAG, "cancelling pairing");
