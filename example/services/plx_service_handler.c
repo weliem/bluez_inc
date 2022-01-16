ffb27c08f0461678d423ebc6416cc8aec037dd11
martijn <martijn.van.welie@gmail.com>
2022-01-16 13:52:10 +0100

    Parse complete PLX features object

--- example/services/plx_service_handler.c
+++ example/services/plx_service_handler.c
@@ -3,6 +3,7 @@
 //
 
 #include "plx_service_handler.h"
+#include "plx_features.h"
 #include "../../characteristic.h"
 #include "../../parser.h"
 #include "../../logger.h"
@@ -12,18 +13,15 @@
 
 #define TAG "PLX_Service_Handler"
 
+typedef struct plx_internal {
+    GHashTable *features;
+} PlxInternal;
+
 static void plx_onCharacteristicsDiscovered(ServiceHandler *service_handler, Device *device, GList *characteristics) {
     log_debug(TAG, "discovered %d characteristics", g_list_length(characteristics));
-    Characteristic *spot_measurement_char = binc_device_get_characteristic(device, PLX_SERVICE_UUID,
-                                                                           SPOT_MEASUREMENT_CHAR_UUID);
-    if (spot_measurement_char != NULL && binc_characteristic_supports_notify(spot_measurement_char)) {
-        binc_characteristic_start_notify(spot_measurement_char);
-    }
-//    Characteristic *continuous_measurement_char = binc_device_get_characteristic(device, PLX_SERVICE_UUID,
-//                                                                                 CONTINUOUS_MEASUREMENT_CHAR_UUID);
-//    if (continuous_measurement_char != NULL && binc_characteristic_supports_notify(continuous_measurement_char)) {
-//        binc_characteristic_start_notify(continuous_measurement_char);
-//    }
+
+    binc_device_read_char(device, PLX_SERVICE_UUID, PLX_FEATURE_CHAR_UUID);
+    binc_device_start_notify(device, PLX_SERVICE_UUID, PLX_SPOT_MEASUREMENT_CHAR_UUID);
 }
 
 static void plx_onNotificationStateUpdated(ServiceHandler *service_handler,
@@ -32,11 +30,12 @@ static void plx_onNotificationStateUpdated(ServiceHandler *service_handler,
                                            const GError *error) {
 
     const char *uuid = binc_characteristic_get_uuid(characteristic);
-    gboolean is_notifying = binc_characteristic_is_notifying(characteristic);
     if (error != NULL) {
         log_debug(TAG, "failed to start/stop notify '%s' (error %d: %s)", uuid, error->code, error->message);
         return;
     }
+
+    gboolean is_notifying = binc_characteristic_is_notifying(characteristic);
     log_debug(TAG, "characteristic <%s> notifying %s", uuid, is_notifying ? "true" : "false");
 }
 
@@ -58,7 +57,7 @@ typedef struct plx_spot_measurement {
     float spo2_value;
     float pulse_value;
     float pulse_amplitude_index;
-    MeasurementStatus* measurement_status;
+    MeasurementStatus *measurement_status;
     guint16 sensor_status;
     GDateTime *timestamp;
     gboolean is_device_clock_set;
@@ -76,7 +75,7 @@ static Observation *plx_spot_measurement_as_observation(SpotMeasurement *measure
     return observation;
 }
 
-static MeasurementStatus* plx_create_measurement_status(guint16 status) {
+static MeasurementStatus *plx_create_measurement_status(guint16 status) {
     MeasurementStatus *measurementStatus = g_new0(MeasurementStatus, 1);
     measurementStatus->isMeasurementOngoing = (status & 0x0020) > 0;
     measurementStatus->isEarlyEstimateData = (status & 0x0040) > 0;
@@ -106,7 +105,8 @@ static SpotMeasurement *plx_create_spot_measurement(const GByteArray *byteArray)
     measurement->spo2_value = parser_get_sfloat(parser);
     measurement->pulse_value = parser_get_sfloat(parser);
     measurement->timestamp = timestampPresent ? parser_get_date_time(parser) : NULL;
-    measurement->measurement_status = plx_create_measurement_status(measurementStatusPresent ? parser_get_uint16(parser) : 0x0000);
+    measurement->measurement_status = measurementStatusPresent ? plx_create_measurement_status(
+            parser_get_uint16(parser)) : NULL;
     measurement->sensor_status = sensorStatusPresent ? parser_get_uint16(parser) : 0xFFFF;
     guint8 reserved_byte = sensorStatusPresent ? parser_get_uint8(parser) : 0xFF;
     measurement->pulse_amplitude_index = pulseAmplitudeIndexPresent ? parser_get_sfloat(parser) : 0x00;
@@ -124,6 +124,50 @@ void plx_spot_measurement_free(SpotMeasurement *measurement) {
     g_free(measurement);
 }
 
+static void handleFeature(const ServiceHandler *service_handler, const Device *device, const GByteArray *byteArray) {
+    PlxFeatures *plxFeatures = plx_create_features(byteArray);
+    g_assert(plxFeatures != NULL);
+
+    PlxInternal *plxInternal = (PlxInternal *) service_handler->private_data;
+    g_assert(plxInternal != NULL);
+
+    g_hash_table_insert(plxInternal->features, g_strdup(binc_device_get_address(device)), plxFeatures);
+    log_debug(TAG, "got PLX feature");
+}
+
+static void
+handleSpotMeasurement(const ServiceHandler *service_handler, const Device *device, const GByteArray *byteArray) {
+    SpotMeasurement *measurement = plx_create_spot_measurement(byteArray);
+
+    if (measurement->timestamp == NULL) {
+        log_debug(TAG, "Ignoring spot measurement without timestamp");
+        return;
+    }
+
+    // Make sure we only send valid spot measurements
+    if (measurement->measurement_status != NULL) {
+        PlxInternal *plxInternal = (PlxInternal *) service_handler->private_data;
+        PlxFeatures *plxFeatures = g_hash_table_lookup(plxInternal->features, binc_device_get_address(device));
+        if (plxFeatures != NULL && plxFeatures->measurementStatusSupport->isFullyQualifiedDataSupported) {
+            if (!measurement->measurement_status->isFullyQualifiedData) {
+                log_debug(TAG, "Ignoring spot measurement because it is not a valid measurement");
+                return;
+            }
+        }
+    }
+
+    Observation *observation = plx_spot_measurement_as_observation(measurement);
+    plx_spot_measurement_free(measurement);
+
+    GList *observation_list = NULL;
+    observation_list = g_list_append(observation_list, observation);
+    if (service_handler->observations_callback != NULL) {
+        DeviceInfo *deviceInfo = get_device_info(binc_device_get_address(device));
+        service_handler->observations_callback(observation_list, deviceInfo);
+    }
+    observation_list_free(observation_list);
+}
+
 static void plx_onCharacteristicChanged(ServiceHandler *service_handler,
                                         Device *device,
                                         Characteristic *characteristic,
@@ -131,45 +175,37 @@ static void plx_onCharacteristicChanged(ServiceHandler *service_handler,
                                         const GError *error) {
 
     const char *uuid = binc_characteristic_get_uuid(characteristic);
-    if (g_str_equal(uuid, SPOT_MEASUREMENT_CHAR_UUID)) {
-        SpotMeasurement *measurement = plx_create_spot_measurement(byteArray);
-
-        if (measurement->timestamp == NULL) {
-            log_debug(TAG, "Ignoring spot measurement without timestamp");
-            return;
-        }
-
-        if (!measurement->measurement_status->isFullyQualifiedData) {
-            log_debug(TAG, "Ignoring spot measurement because it is not a valid measurement");
-            return;
-        }
-
-        Observation *observation = plx_spot_measurement_as_observation(measurement);
-        plx_spot_measurement_free(measurement);
-
-        GList *observation_list = NULL;
-        observation_list = g_list_append(observation_list, observation);
-        if (service_handler->observations_callback != NULL) {
-            DeviceInfo *deviceInfo = get_device_info(binc_device_get_address(device));
-            service_handler->observations_callback(observation_list, deviceInfo);
-        }
-        observation_list_free(observation_list);
-    }
+    g_str_equal(uuid, PLX_FEATURE_CHAR_UUID) ? handleFeature(service_handler, device, byteArray) :
+    g_str_equal(uuid, PLX_SPOT_MEASUREMENT_CHAR_UUID) ? handleSpotMeasurement(service_handler, device, byteArray) : NULL;
 }
 
 static void plx_free(ServiceHandler *service_handler) {
+    PlxInternal *plxInternal = (PlxInternal *) service_handler->private_data;
+    g_hash_table_destroy(plxInternal->features);
+    g_free(plxInternal);
     log_debug(TAG, "freeing HTS private_data");
 }
 
+static void plx_onDeviceDisconnected(ServiceHandler *service_handler, Device *device) {
+    PlxInternal *plxInternal = (PlxInternal *) service_handler->private_data;
+    PlxFeatures *plxFeatures = g_hash_table_lookup(plxInternal->features, binc_device_get_address(device));
+    if (plxFeatures != NULL) {
+        g_hash_table_remove(plxInternal->features, binc_device_get_address(device));
+    }
+}
+
 ServiceHandler *plx_service_handler_create() {
+    PlxInternal *plxInternal = g_new0(PlxInternal, 1);
+    plxInternal->features = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, (GDestroyNotify) plx_free_features);
+
     ServiceHandler *handler = g_new0(ServiceHandler, 1);
-    handler->private_data = NULL;
+    handler->private_data = plxInternal;
     handler->uuid = PLX_SERVICE_UUID;
     handler->on_characteristics_discovered = &plx_onCharacteristicsDiscovered;
     handler->on_notification_state_updated = &plx_onNotificationStateUpdated;
     handler->on_characteristic_write = NULL;
     handler->on_characteristic_changed = &plx_onCharacteristicChanged;
-    handler->on_device_disconnected = NULL;
+    handler->on_device_disconnected = &plx_onDeviceDisconnected;
     handler->service_handler_free = &plx_free;
     return handler;
 }
