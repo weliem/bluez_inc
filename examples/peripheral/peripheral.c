96db2842532544311e518ff155078d684f9c7261
martijn <martijn.van.welie@gmail.com>
2022-04-05 19:39:23 +0200

    Start and stop advertising based on whether remote central is connected or not

--- examples/peripheral/peripheral.c
+++ examples/peripheral/peripheral.c
@@ -50,6 +50,12 @@ void on_powered_state_changed(Adapter *adapter, gboolean state) {
 
 void on_central_state_changed(Adapter *adapter, Device *device) {
     log_debug(TAG, "remote central %s is %s", binc_device_get_address(device), binc_device_get_connection_state_name(device));
+    ConnectionState state = binc_device_get_connection_state(device);
+    if (state == CONNECTED) {
+        binc_adapter_stop_advertising(adapter, advertisement);
+    } else {
+        binc_adapter_start_advertising(adapter, advertisement);
+    }
 }
 
 void on_local_char_read(const Application *application, const char *address, const char *service_uuid,
